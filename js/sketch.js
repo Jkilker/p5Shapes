4ce6ac5245a17db339499d403954d064f7f5a1c3
Jkilker <116875574+Jkilker@users.noreply.github.com>
2022-10-28 10:19:37 +0100

    Add files via upload
    we have added comments about p5 functions and we have added some shapes and colours

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,16 @@
+//setup function runs once at the start
 function setup() {
     var canvas = createCanvas(800, 800);
     canvas.parent("p5container");
 }
-
+//draw function is a continous loop, runs from the start, at 60fps
 function draw() {
-    background(220);
+    background(220); //light grey background
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    fill(255, 255, 255); //setting fill to rgb value for white
+    ellipse(400, 400, 200); //circle beinng drawn on our canvas
+    
+    fill(51)
+    square(300,300,100)
+    
 }
